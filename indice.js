b5adc85a81e8cf29f8889ba5fd8794a069180caf
Javier184824 <64340244+Javier184824@users.noreply.github.com>
2026-03-31 18:10:47 -0600

    Update indice.js

--- indice.js
+++ indice.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/courses', courseRoutes);
