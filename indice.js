75214a6b673092c07fee6f7475c2a2eab0c72c67
Javier184824 <leejavier62@gmail.com>
2026-04-04 15:27:36 -0600

    Update indice.js

--- indice.js
+++ indice.js
@@ -36,9 +36,9 @@ app.use('/api/users', userRoutes);
 app.use('/api/files', fileRoutes);
 app.use('/api/admin', adminRoutes);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/inicio.html'));
-});
+//app.get('/', (req, res) => {
+//    res.sendFile(path.join(__dirname, 'public/inicio.html'));
+//});
 
 app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
     console.log(`TEC Digitalito corriendo en puerto ${process.env.PORT || 3000}`);
