f29fcf49c2be3daafe792a398a2813a49e35edb8
Javier184824 <leejavier62@gmail.com>
2026-04-19 23:54:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -67,6 +67,9 @@ const test = async () => {
   await mongoose.connect(process.env.MONGODB_URI);
   gfsBucket = new GridFSBucket(mongoose.connection.db, { bucketName: 'uploads' });
   console.log('MongoDB: conectado al Replica Set');
+
+  const collections = await mongoose.connection.db.listCollections().toArray();
+  console.log(collections.map(c => c.name));
 }
 
 //start();
